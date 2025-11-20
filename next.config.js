0b1a0edc51254ba705e3a42836f00174ded179d9
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-20 12:11:53 +0100

    fix: type in next config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,12 @@ const nextConfig = {
       {
         // Przekieruj samo /docs na /docs w drugiej apce
         source: "/docs",
-        destination: "https://docs-chi-blond.vercel.app//docs",
+        destination: "https://docs-chi-blond.vercel.app/docs",
       },
       {
         // Przekieruj wszystko głębiej (/docs/intro, /docs/api itp.)
         source: "/docs/:path*",
-        destination: "https://docs-chi-blond.vercel.app//docs/:path*",
+        destination: "https://docs-chi-blond.vercel.app/docs/:path*",
       },
     ]
   },
