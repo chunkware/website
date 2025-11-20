d9cae281d291ccdb935a7c669d58b477bd0e0f4e
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-20 12:09:26 +0100

    feat: add docs rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enable src directory
-};
+  async rewrites() {
+    return [
+      {
+        // Przekieruj samo /docs na /docs w drugiej apce
+        source: "/docs",
+        destination: "https://docs-chi-blond.vercel.app//docs",
+      },
+      {
+        // Przekieruj wszystko głębiej (/docs/intro, /docs/api itp.)
+        source: "/docs/:path*",
+        destination: "https://docs-chi-blond.vercel.app//docs/:path*",
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
