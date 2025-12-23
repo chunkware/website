31674a3c1df4d59566ab8f1c64b08cff76cfcb6d
kkazzann <kamil.kazaniecki@beliani.net>
2025-12-23 10:04:52 +0100

    feat: add theme extend with our colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        accent: '#48A0F3',
+        'main': '#1F1D1D',
+        'surface': '#0b0b0d'
+      },
+    },
+  },
   plugins: [],
 }
